d4464c246e71a95a13e861ff3c5c89db53266923
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2021-09-23 23:40:03 -0600

    updating logging to connections

--- api/src/constants/index.ts
+++ api/src/constants/index.ts
@@ -1,1 +1,2 @@
 export const port = 5001
+export const maxRetriesToConnect = 3
--- api/src/db/mapExistingConnections.ts
+++ api/src/db/mapExistingConnections.ts
@@ -2,10 +2,22 @@ import Redis, { Redis as IRedis } from 'ioredis'
 
 import { AppConfig, MonitoringStatus } from '@/interfaces'
 import { connections, monitoredConnections } from '@/connections'
+import { maxRetriesToConnect } from '@/constants'
+import { logger } from '@/utils/logger'
 
 export const mapExistingConnections = ({ connections: existingConnections }: AppConfig): void => {
-  existingConnections.map(({ id, host, port, password }) => {
-    connections[id] = new Redis({ host, password: password || undefined, port })
+  existingConnections.forEach(({ id, host, port, password }) => {
+    connections[id] = new Redis({
+      host,
+      password: password || undefined,
+      port,
+      retryStrategy: times => {
+        const canRetry = times <= maxRetriesToConnect
+        if (canRetry) return 1
+        logger.error(`Attempted to connect to ${host} ${maxRetriesToConnect} times unsuccessfully.`)
+        return null
+      }
+    })
   })
 }
 
