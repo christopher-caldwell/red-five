39ed53be5986ad1359cb5cda1400440788bdd376
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2022-06-12 10:46:27 -0500

    fix: Running

--- ui/src/components/header/server-dropdown/connection-display/index.tsx
+++ ui/src/components/header/server-dropdown/connection-display/index.tsx
@@ -1,16 +1,16 @@
 import { FC } from 'react'
-import { DataGrid, GridColDef } from '@material-ui/data-grid'
+// import { DataGrid, GridColDef } from '@material-ui/data-grid'
 import { Alert, LinearProgress } from '@mui/material'
 
-import EditConnection from './actions/Edit'
-import RemoveConnection from './actions/Remove'
-import MakeActive from './actions/MakeActive'
-import { useConnectionsQuery, Connection } from '@/generated'
+// import EditConnection from './actions/Edit'
+// import RemoveConnection from './actions/Remove'
+// import MakeActive from './actions/MakeActive'
+import { useConnectionsQuery } from '@/generated'
 import { Container, DataGridOuterContainer, DataGridInnerContainer } from './elements'
 
 const ConnectionDisplay: FC = () => {
-  const { isLoading, data, isError } = useConnectionsQuery()
-  const connections: Connection[] = data?.connections || []
+  const { isLoading, isError } = useConnectionsQuery()
+  // const connections: Connection[] = data?.connections || []
   return (
     <Container elevation={2}>
       {isError ? (
@@ -22,7 +22,7 @@ const ConnectionDisplay: FC = () => {
       <div>
         <DataGridOuterContainer>
           <DataGridInnerContainer>
-            <DataGrid disableColumnSelector disableSelectionOnClick columns={columns} rows={connections} />
+            {/* <DataGrid disableColumnSelector disableSelectionOnClick columns={columns} rows={connections} /> */}
           </DataGridInnerContainer>
         </DataGridOuterContainer>
       </div>
@@ -30,37 +30,37 @@ const ConnectionDisplay: FC = () => {
   )
 }
 
-const columns: GridColDef[] = [
-  {
-    field: 'isActive',
-    headerName: 'Active',
-    flex: 0.25,
-    disableColumnMenu: true,
-    sortable: false,
-    renderCell: params => <MakeActive {...(params.row as Connection)} />
-  },
-  { field: 'name', headerName: 'Name', flex: 0.35, disableColumnMenu: true },
-  {
-    field: 'host',
-    headerName: 'Host',
-    flex: 0.6
-  },
-  {
-    field: 'edit',
-    headerName: 'Edit',
-    headerAlign: 'center',
-    flex: 0.3,
-    disableColumnMenu: true,
-    sortable: false,
-    renderCell: ({ id }) => {
-      return (
-        <>
-          <EditConnection id={id} />
-          <RemoveConnection id={id} />
-        </>
-      )
-    }
-  }
-]
+// const columns: GridColDef[] = [
+//   {
+//     field: 'isActive',
+//     headerName: 'Active',
+//     flex: 0.25,
+//     disableColumnMenu: true,
+//     sortable: false,
+//     renderCell: params => <MakeActive {...(params.row as Connection)} />
+//   },
+//   { field: 'name', headerName: 'Name', flex: 0.35, disableColumnMenu: true },
+//   {
+//     field: 'host',
+//     headerName: 'Host',
+//     flex: 0.6
+//   },
+//   {
+//     field: 'edit',
+//     headerName: 'Edit',
+//     headerAlign: 'center',
+//     flex: 0.3,
+//     disableColumnMenu: true,
+//     sortable: false,
+//     renderCell: ({ id }) => {
+//       return (
+//         <>
+//           <EditConnection id={id} />
+//           <RemoveConnection id={id} />
+//         </>
+//       )
+//     }
+//   }
+// ]
 
 export default ConnectionDisplay
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,18 +1,16 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import Checker from 'vite-plugin-checker'
 import { resolve } from 'path'
+import { UserConfig, Plugin } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 function pathResolve(dir: string) {
   return resolve(__dirname, '.', dir)
 }
 
-// https://vitejs.dev/config/
-const config = () => ({
-  esbuild: {
-    jsxFactory: '_jsx',
-    jsxFragment: '_jsxFragment',
-    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`
-  },
+const shouldAnalyze = process.env.ANALYZE
+
+const config: UserConfig = {
   resolve: {
     alias: [
       {
@@ -21,8 +19,43 @@ const config = () => ({
       }
     ]
   },
+  esbuild: {
+    jsxFactory: '_jsx',
+    jsxFragment: '_jsxFragment',
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`
+  },
+  build: {
+    rollupOptions: {
+      plugins: shouldAnalyze ? [visualizer({ open: true, filename: './bundle-size/bundle.html' }) as Plugin] : []
+    },
+    sourcemap: !!shouldAnalyze
+  },
   plugins: [
-    reactRefresh(),
+    react({
+      babel: {
+        plugins: [
+          [
+            '@emotion',
+            {
+              importMap: {
+                '@mui/material': {
+                  styled: {
+                    canonicalImport: ['@emotion/styled', 'default'],
+                    styledBaseImport: ['@mui/material', 'styled']
+                  }
+                },
+                '@mui/material/styles': {
+                  styled: {
+                    canonicalImport: ['@emotion/styled', 'default'],
+                    styledBaseImport: ['@mui/material/styles', 'styled']
+                  }
+                }
+              }
+            }
+          ]
+        ]
+      }
+    }),
     Checker({
       typescript: true,
       overlay: true,
@@ -32,6 +65,8 @@ const config = () => ({
       }
     })
   ]
-})
+}
 
-export default config
+const getConfig = () => config
+
+export default getConfig
