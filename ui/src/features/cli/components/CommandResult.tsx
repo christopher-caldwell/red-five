d64e24269a76a8104df668e50d217fefcac0fae2
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2022-06-13 12:10:18 -0500

    fix: Restructure

--- ui/src/features/cli/components/CommandResult.tsx
+++ ui/src/features/cli/components/CommandResult.tsx
@@ -1,13 +1,13 @@
-import { FC, useMemo, memo } from 'react'
+import { FC, useMemo } from 'react'
 import ChevronRight from '@mui/icons-material/ChevronRight'
-import deepEqual from 'fast-deep-equal'
+
+import { styled } from '@mui/material'
 
 import { standardFormatDate } from '@/utils/dates'
 import { FlexContainer } from '@/components'
 import { CliResponse } from '@/generated'
-import { MessageContainer, CommandTitle, Container, TimeContainer } from './elements'
 
-const CommandResult: FC<Props> = ({ time, message, command }) => {
+export const CommandResult: FC<Props> = ({ time, message, command }) => {
   const formattedTime = useMemo(() => standardFormatDate(time), [time])
   return (
     <Container>
@@ -23,6 +23,18 @@ const CommandResult: FC<Props> = ({ time, message, command }) => {
   )
 }
 
-interface Props extends CliResponse {}
+const CommandTitle = styled('h3')`
+  color: ${({ theme }) => theme.palette.primary.main};
+`
+const Container = styled('div')``
 
-export default memo(CommandResult, deepEqual)
+const TimeContainer = styled(FlexContainer)`
+  margin-left: 20px;
+`
+
+const MessageContainer = styled('div')`
+  width: 100%;
+  padding: 10px 0;
+`
+
+interface Props extends CliResponse {}
--- ui/src/features/cli/index.tsx
+++ ui/src/features/cli/index.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react'
 import { Virtuoso } from 'react-virtuoso'
+import { styled } from '@mui/material'
 
-import { Window } from './elements'
-import CommandResult from './command-result'
-import { useCliWindow, Props } from './useCliWindow'
+import { CommandResult } from './components'
+import { useCliWindow, Props } from './api'
 
 const CLIWindow: FC<Props> = ({ response }) => {
   const { messages } = useCliWindow(response)
@@ -21,5 +21,24 @@ const CLIWindow: FC<Props> = ({ response }) => {
     </Window>
   )
 }
+
+const Window = styled('div')`
+  width: 100%;
+  /* top: 150px; */
+  border: 0.5px solid gray;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  /* align-items: flex-start;
+  justify-content: flex-start;
+  flex-flow: column-reverse; */
+  padding: 1%;
+  overflow: scroll;
+  height: 65vh;
+  margin-top: 10px;
+  @media screen and (min-width: 1500px) {
+    height: 70vh;
+  }
+`
 
 export default CLIWindow
