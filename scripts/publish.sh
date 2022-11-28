d6a85db0140168eb264f9bda287072a3be96b124
christopher-caldwell <well.done.514@gmail.com>
2022-11-28 14:59:41 -0600

    fix: Uncommenting

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -26,10 +26,10 @@ fi
 
 printf "\n\n$BICyan$(echo Bumping the version number..)$Color_Off"
 
-# standard-version
+standard-version
 
 printf "\n\n$BICyan$(echo Publishing new version..)$Color_Off"
 
-# git push --follow-tags origin master
+git push --follow-tags origin master
 
-# yarn publish --access=public
+yarn publish --access=public
